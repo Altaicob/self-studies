31f76e6f2d2bd7f9472253cd8ef759bfe8ba4e78
Altaicob <114251333+Altaicob@users.noreply.github.com>
2024-10-30 08:12:24 +0300

    Add files via upload

---
+++ Calculator1.1/Calculator1.1/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Calculator1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char opr;
+            float first, second;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter an operator(+,-,*,/): ");
+                    opr = Console.ReadLine()[0];
+
+                    //Contains method checks
+                    if ("+-*/".Contains(opr))
+                        break;
+
+                }
+                catch { }
+                Console.WriteLine("The operator is invalid.");
+            }
+
+            while (true) 
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number: ");
+                    first = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch { }
+                Console.WriteLine("Please enter a valid number.");
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number: ");
+                    second = float.Parse(Console.ReadLine());
+                    break;
+                }
+                catch { }
+                    Console.WriteLine("Please enter a valid number.");
+            }
+
+            float result = 0;
+            if (opr == '+')
+                result = first + second;
+            else if (opr == '-')
+                result = first - second;
+            else if (opr == '/')
+                result = first / second;
+            else if (opr == '*')
+                result = first * second;
+
+            Console.WriteLine($"Result of '{first} {opr} {second}' is: {result}");
+        }
+    }
+}
