ee4f6efca9c61f8862c0a698a06eee60cd5c4297
Altaicob <114251333+Altaicob@users.noreply.github.com>
2024-10-30 08:11:29 +0300

    Add files via upload

---
+++ asynchronous/asynchronous/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace asynchronous
+{
+    public class program
+    {
+
+        static void Main()
+        {
+            System.Console.WriteLine(">> Start");
+            performOperation("Task1", 2000);
+            performOperation("Task2", 3000);
+            System.Console.WriteLine("<< End");
+        }
+
+        static void performOperation(string taskName, int delayDuration)
+        {
+            System.Console.WriteLine($"{taskName} started.");
+            Thread.Sleep(delayDuration);
+            System.Console.WriteLine($"{taskName} finished.");
+        }
+    }
+}
